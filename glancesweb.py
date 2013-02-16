0375d86b420f5883f582c98beb3fe743d469aaad
Sakti Dwi Cahyono <54krpl@gmail.com>
2013-02-17 00:10:17 +0700

    add static files, add route to get all status from glances

--- glancesweb.py
+++ glancesweb.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import Response
+from xmlrpclib import ServerProxy
 
 
 app = Flask(__name__)
+SERVER_HOST = "localhost"
+SERVER_PORT = "61209"
 
 
 @app.route("/")
@@ -9,5 +13,11 @@ def dashboard():
     return "Monitoring Dashboard"
 
 
+@app.route("/status.json")
+def get_status():
+    server = ServerProxy("http://%s:%s" % (SERVER_HOST, SERVER_PORT))
+    data = server.getAll()
+    return Response(data, mimetype="application/json")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
