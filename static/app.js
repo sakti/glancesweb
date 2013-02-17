33cdfcf1829d9131e1e8324e74f531178206dc25
Sakti Dwi Cahyono <54krpl@gmail.com>
2013-02-17 10:00:10 +0700

    add gauge vis

--- static/app.js
+++ static/app.js
@@ -1,12 +1,45 @@
 $(function(){
+
+	var gauges = [];
+			
+	function createGauge(name, label)
+	{
+		var config = 
+		{
+			size: 160,
+			label: label,
+			minorTicks: 5
+		}
+		
+		config.redZones = [];
+		config.redZones.push({ from: 90, to: 100 });
+
+		config.yellowZones = [];
+		config.yellowZones.push({ from: 75, to: 90 });
+		
+		gauges[name] = new Gauge(name, config);
+		gauges[name].render();
+	}
+
+	
+	createGauge("cpu-idle", "CPU Idle");
+	createGauge("cpu-iowait", "CPU IO wait");
+	createGauge("cpu-irq", "CPU IRQ");
+	createGauge("cpu-nice", "CPU Nice");
+	createGauge("cpu-system", "CPU System");
+	createGauge("cpu-user", "CPU User");
+
+	createGauge("memory-usage", "Mem Usage")
+
 	render = function(data) {
 		console.log(data);
-		$('#cpu-idle').text(data.cpu.idle)
-		$('#cpu-iowait').text(data.cpu.iowait)
-		$('#cpu-irq').text(data.cpu.irq)
-		$('#cpu-nice').text(data.cpu.nice)
-		$('#cpu-system').text(data.cpu.system)
-		$('#cpu-user').text(data.cpu.user)
+		gauges['cpu-idle'].redraw(data.cpu.idle);
+		gauges['cpu-iowait'].redraw(data.cpu.iowait);
+		gauges['cpu-irq'].redraw(data.cpu.irq);
+		gauges['cpu-nice'].redraw(data.cpu.nice);
+		gauges['cpu-system'].redraw(data.cpu.system);
+		gauges['cpu-user'].redraw(data.cpu.user);
+		gauges['memory-usage'].redraw(data.mem.percent);
 	}
 
 	if (window.glances_server){
