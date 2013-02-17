ae9c660bb6ed91f77decdebd1697592c42392a75
Sakti Dwi Cahyono <54krpl@gmail.com>
2013-02-17 09:11:38 +0700

    add setting page, reorganize template structure

--- glancesweb.py
+++ glancesweb.py
@@ -1,22 +1,46 @@
 from flask import Flask
 from flask import Response
+from flask import render_template
+from flask import session
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import abort
 from xmlrpclib import ServerProxy
+import socket
 
 
 app = Flask(__name__)
-SERVER_HOST = "localhost"
-SERVER_PORT = "61209"
+app.secret_key = "development key"
+GLANCES_SERVER = "localhost:61209"
 
 
 @app.route("/")
 def dashboard():
-    return "Monitoring Dashboard"
+    if "glances_server" not in session:
+        return redirect(url_for("setting"))
+    return render_template("dashboard.html")
+
+
+@app.route("/setting", methods=["GET", "POST"])
+def setting():
+    if request.method == "POST":
+        server = request.form["server"]
+        port = request.form["port"]
+        session["glances_server"] = "%s:%s" % (server, port)
+        return redirect(url_for("dashboard"))
+    return render_template("setting.html")
 
 
 @app.route("/status.json")
 def get_status():
-    server = ServerProxy("http://%s:%s" % (SERVER_HOST, SERVER_PORT))
-    data = server.getAll()
+    if "glances_server" not in session:
+        abort(401)
+    server = ServerProxy("http://%s" % GLANCES_SERVER)
+    try:
+        data = server.getAll()
+    except socket.error:
+        data = ''
     return Response(data, mimetype="application/json")
 
 if __name__ == "__main__":
